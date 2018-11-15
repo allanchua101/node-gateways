4a3191c9a869d6886fe23c1247e5c4558e63200b
Allan Chua <allanchua.officefiles@gmail.com>
2018-11-15 23:58:33 +0800

    -Implemented helpful guide for downstream service dependency installer script

--- 001_Setup/Install Downstream Services.sh
+++ 001_Setup/Install Downstream Services.sh
@@ -1,15 +1,21 @@
 cd ./0001-users
 
+echo 'Installing Users API Service....'
 npm install
 
 cd ..
 
 cd ./0002-products
 
+echo 'Installing Products API Service....'
 npm install
 
-cd ...
+cd ..
 
+echo 'Installing Transactions API Service....'
 cd ./0003-transactions
 
 npm install
+
+echo 'Installation Completed! Press any key to continue....'
+read
