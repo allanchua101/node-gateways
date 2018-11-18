4823c0db4ba381790844c40ec7d7c40e0c73b3a4
Allan Chua <allanchua.officefiles@gmail.com>
2018-11-18 20:49:00 +0800

    -Implemented transactions demo

---
+++ 001_Setup/0003-transactions/routes/transactions.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET transactions. */
+router.get('/', function(req, res, next) {
+  res.json([
+    {
+      id: 1,
+      customerID: 1,
+      // Context independence
+      productID: 1,
+      productName: "VueJS T-Shirt",
+      quantity: 5,
+      priceAtTimeOfSale: 75
+    }, 
+    {
+      id: 2,
+      customerID: 1,
+      productID: 1,
+      productName: "VueJS T-Shirt",
+      quantity: 10,
+      priceAtTimeOfSale: 100
+    }
+  ]);
+});
+
+module.exports = router;
