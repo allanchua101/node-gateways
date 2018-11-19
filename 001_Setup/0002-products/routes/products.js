e2882f869f508442889ae1e76c07ca2d14766b2e
Allan Chua <allanchua.officefiles@gmail.com>
2018-11-19 23:02:53 +0800

    -Setup gateway proxies

--- 001_Setup/0002-products/routes/products.js
+++ 001_Setup/0002-products/routes/products.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET products listing. */
-router.get('/', function(req, res, next) {
+router.get('/list', function(req, res, next) {
   res.json([
     {
       id: 1,
